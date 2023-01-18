5b94e57aaad05c5c67d14b3f5a11c9d8eed3009f
nazarkhanov <joni.mypth@gmail.com>
2023-01-18 12:36:09 +0600

    ref: make elements sizes more comfortable on small screens

--- src/client/routes/Settings.jsx
+++ src/client/routes/Settings.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 
 export default () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-14 xl:container xl:mx-auto px-3 pt-10 pb-5 sm:px-20 sm:py-16 max-sm:-mx-3 max-sm:-mt-4 rounded-xl shadow-md shadow-slate-200 bg-white">
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-14 xl:container xl:mx-auto px-5 pt-10 pb-5 sm:px-20 sm:py-16 max-sm:-mx-6 max-sm:-mt-4 rounded-xl shadow-md shadow-slate-200 bg-white">
       <div>
         <Section title="Персональные данные">
           <div className="flex items-center mb-10">
             <img src="/users/1.png" alt="#" className="w-12 h-12 lg:w-14 lg:h-14 rounded-full" />
             <span className="flex flex-col text-left ml-6 sm:ml-7">
-              <span className="font-semibold text-lg">KazGazProm</span>
-              <span className="font-normal text-sm">Kazgazprom@email.com</span>
+              <span className="font-semibold text-lg sm:text-lg">KazGazProm</span>
+              <span className="font-normal text-xs sm:text-sm">Kazgazprom@email.com</span>
             </span>
           </div>
 
-          <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 gap-x-6 gap-y-2 sm:gap-6">
             <Field name="Имя">
               <Input type="text" defaultValue="Нуртас" />
             </Field>
@@ -34,7 +34,7 @@ export default () => {
 
       <div>
         <Section title="Контактная информация">
-          <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 gap-x-6 gap-y-2 sm:gap-6">
             <Field name="Номер телефона">
               <Input type="text" defaultValue="+7-707-787-8778" />
             </Field>
@@ -74,7 +74,7 @@ function Section({ title, children }) {
   return (
     <div className="mb-8">
       <div className="uppercase text-gray-600 text-sm sm:text-base font-semibold mb-4">{title}</div>
-      <div className="border-gray-200 border-y-2 sm:border-2 sm:rounded-xl px-3 sm:px-8 py-8 max-sm:-mx-3 ">
+      <div className="border-gray-200 border-y-2 sm:border-2 sm:rounded-xl px-5 sm:px-8 py-8 max-sm:-mx-6 ">
         {children}
       </div>
     </div>
@@ -85,7 +85,7 @@ function Field({ tag, name, className, children }) {
   const Wrapper = tag ?? "label";
 
   return (
-    <Wrapper className={`flex flex-col text-gray-600 text-sm sm:text-base py-2 ${className}`}>
+    <Wrapper className={`flex flex-col text-gray-600 text-xs sm:text-base py-2 ${className}`}>
       <span>{name}</span>
       {children}
     </Wrapper>
@@ -98,7 +98,7 @@ function Input({ className, ...props }) {
 
   return (
     <input
-      className={`text-base text-black border-2 mt-2 px-4 py-[10px] bg-zinc-100 focus:bg-zinc-200 focus:outline-none transition ease-in-out duration-150 rounded ${
+      className={`text-sm sm:text-base text-black border-2 mt-2 px-3 py-[10px] bg-zinc-100 focus:bg-zinc-200 focus:outline-none transition ease-in-out duration-150 rounded ${
         currentValue === defaultValue ? "border-zinc-200" : "border-emerald-700"
       }`}
       onChange={(e) => setCurrentValue(e.target.value)}
@@ -111,7 +111,7 @@ function Button({ className, ...props }) {
   return (
     <button
       type="button"
-      className={`block rounded-full text-center text-base px-8 py-3 bg-zinc-900 text-white font-semibold select-none ${className}`}
+      className={`block rounded-full text-center text-sm sm:text-base px-8 py-3 bg-zinc-900 text-white font-semibold select-none ${className}`}
       {...props}
     >
       {props.children}
@@ -137,7 +137,7 @@ function FileInput({ className, ...props }) {
   return (
     <>
       <label
-        className={`flex items-center text-base text-black whitespace-nowrap border-2 mt-2 pl-3 pr-4 py-[10px] bg-zinc-100 hover:bg-zinc-200 transition ease-in-out duration-150 rounded cursor-pointer ${
+        className={`flex items-center text-base text-black whitespace-nowrap border-2 mt-2 px-2 sm:pl-3 sm:pr-4 py-[10px] bg-zinc-100 hover:bg-zinc-200 transition ease-in-out duration-150 rounded cursor-pointer ${
           selectedFile === defaultFile ? "border-zinc-200" : "border-emerald-700"
         }`}
       >
@@ -152,7 +152,7 @@ function FileInput({ className, ...props }) {
           {...props}
         />
 
-        <span className="ml-3 grow-[1] overflow-hidden">
+        <span className="ml-2 sm:ml-3 grow-[1] overflow-hidden text-sm sm:text-base">
           {selectedFile ? selectedFile.name : "Не выбрано"}
         </span>
       </label>
@@ -180,7 +180,7 @@ function FileIcon(props) {
         fillRule="evenodd"
         d="M5 0a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7.81a5 5 0 0 0-1.159-3.2L18.5 1.8A5 5 0 0 0 14.66 0H5Zm1.64 15.5a.64.64 0 0 0-.64.64v.32c0 .354.287.64.64.64h10.72a.64.64 0 0 0 .64-.64v-.32a.64.64 0 0 0-.64-.64H6.64ZM6 11.64a.64.64 0 0 1 .64-.64h10.72a.64.64 0 0 1 .64.64v.32a.64.64 0 0 1-.64.64H6.64a.64.64 0 0 1-.64-.64v-.32Zm.64-5.14a.64.64 0 0 0-.64.64v.32c0 .353.287.64.64.64h7.72a.64.64 0 0 0 .64-.64v-.32a.64.64 0 0 0-.64-.64H6.64Z"
         clipRule="evenodd"
-      />{" "}
+      />
     </svg>
   );
 }
