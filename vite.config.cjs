e705208c0efd26c086f73fbe15dfd8c7995681ac
nazarkhanov <joni.mypth@gmail.com>
2023-01-20 12:19:42 +0600

    feat: add dot env processing

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,14 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import postcss_tailwindcss from "tailwindcss";
 import postcss_autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [postcss_tailwindcss, postcss_autoprefixer],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [postcss_tailwindcss, postcss_autoprefixer],
+      },
     },
-  },
+    server: {
+      proxy: {
+        // with options: http://localhost:5173/api -> http://localhost:8000/api
+        "/api": {
+          target: process.env.VITE_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
