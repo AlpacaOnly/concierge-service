04ecfdb4dd2d4085a1a5b1436d9ee0e8cb716b32
nazarkhanov <joni.mypth@gmail.com>
2023-01-22 10:22:55 +0600

    ref: roles api

---
+++ src/common/api/roles.js
@@ -0,0 +1,11 @@
+const roles = {
+  ADMIN: 1,
+  MANAGER: 2,
+  CLIENT: 3,
+  COMPANY: 4,
+  PARTNER: 5,
+};
+
+export const useRoles = () => {
+  return roles;
+};
--- src/common/components/AppRoutes.jsx
+++ src/common/components/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { default as React } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import loadable from "@loadable/component";
 import { userHooks } from "../api/user";
+import { useRoles } from "../api/roles";
 
 const LoginRoute = loadable(() => import("../routes/Login"));
 const NotFoundRoute = loadable(() => import("../routes/NotFound"));
@@ -13,13 +14,14 @@ const ClientLayout = loadable(() => import("../../client/components/Layout"));
 
 export const AppRoutes = () => {
   const user = userHooks.useUser();
-  const role = user.get()?.role ?? "";
+  const roles = useRoles();
+  const role = user.get()?.role ?? 0;
 
   return (
     <Routes>
       <Route index element={<LoginRoute />} />
 
-      {role === "client" && (
+      {role === roles.CLIENT && (
         <Route path="panel" element={<ClientLayout />}>
           <Route index element={<ClientSettingsRoute />} />
           <Route path="settings" element={<ClientSettingsRoute />} />
@@ -28,9 +30,10 @@ export const AppRoutes = () => {
           <Route path="reference" element={<ClientReferenceRoute />} />
         </Route>
       )}
-      {role === "admin" && <Route path="panel" element={"admin"}></Route>}
-      {role === "company" && <Route path="panel" element={"company"}></Route>}
-      {role === "partner" && <Route path="panel" element={"partner"}></Route>}
+      {role === roles.ADMIN && <Route path="panel" element={"admin"}></Route>}
+      {role === roles.MANAGER && <Route path="panel" element={"manager"}></Route>}
+      {role === roles.COMPANY && <Route path="panel" element={"company"}></Route>}
+      {role === roles.PARTNER && <Route path="panel" element={"partner"}></Route>}
 
       <Route path="*" element={<NotFoundRoute />} />
     </Routes>
