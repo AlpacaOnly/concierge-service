9925d2825bdbfba7ff5c99860a28edb17ea1f2b4
Mercurial5 <dias.nespayev@gmail.com>
2023-01-26 14:37:33 +0600

    feat: fix admin layouts

--- src/admin/components/Layout.jsx
+++ src/admin/components/Layout.jsx
@@ -1,16 +1,21 @@
-import {Panel} from "../../admin/routes/Management" 
 import {Header} from "../routes/Header"
-import { LeftSideBar } from "../../admin/routes/Management";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
+import {LeftSideBar} from "../routes/Management.jsx";
 
 export default () => {
     return (
         <>
-        <div className="bg-slate-200 min-h-screen">
-            <Header/>
-            <Panel/>
-        </div>
-        
+            <div className="bg-slate-200 min-h-screen">
+                <Header/>
+
+                <div className="py-10 px-6 sm:px-10">
+                    <div className="flex flex-1 overflow-hidden h-screen max-w-screen-2xl mt-8 pb-6 pl-8">
+                        <LeftSideBar/>
+                        <Outlet/>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
