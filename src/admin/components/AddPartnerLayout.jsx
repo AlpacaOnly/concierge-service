58e3e78d150b28759a4eac044dabe2a5bec50ff6
AlpacaOnly <ayazhan1121@gmail.com>
2023-01-26 18:41:45 +0600

    fix errors

--- src/admin/components/AddPartnerLayout.jsx
+++ src/admin/components/AddPartnerLayout.jsx
@@ -5,24 +5,24 @@ export default () => {
     const navigate = useNavigate();
     return (
         <>
-        <div class="p-6 flex items-center justify-center">
-        <div class="container max-w-screen-lg mx-auto">
+        <div className="p-6 flex items-center justify-center">
+        <div className="container max-w-screen-lg mx-auto">
             
-            <div class="bg-gray-100 rounded-xl shadow-lg p-4 px-4 md:p-8 mb-6">
-                <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-                <div class="text-gray-600">
-                    <p class="font-medium text-xl">Добавить партнера</p>
+            <div className="bg-gray-100 rounded-xl shadow-lg p-4 px-4 md:p-8 mb-6">
+                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1">
+                <div className="text-gray-600">
+                    <p className="font-medium text-xl">Добавить партнера</p>
                     <p>Заполните все поля</p>
                 </div>
 
-                <div class="lg:col-span-2">
-                    <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-7">
-                    <div class="md:col-span-6 ">
+                <div className="lg:col-span-2">Name
+                    <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-7">
+                    <div className="md:col-span-6 ">
                         <label for="full_name">Название компании</label>
                         <input type="text" name="full_name" id="full_name" class="outline-none h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
                     </div>
 
-                    <div class="md:col-span-3">
+                    <div className="md:col-span-3">
                         <label for="OrganizationForm">Форма Организации</label>
                         <select name="OrganizationForm" id="address" class="outline-none h-10 border mt-1 rounded px-4 w-full bg-gray-50">
                             <option>ТОО</option>
@@ -32,17 +32,17 @@ export default () => {
                         </select>
                     </div>
 
-                    <div class="md:col-span-3">
+                    <div className="md:col-span-3">
                         <label for="email">Почта</label>
                         <input type="text" name="email" id="email" class="outline-none h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="компания@gmail.com" />
                     </div>
 
-                    <div class="md:col-span-2">
+                    <div className="md:col-span-2">
                         <label for="IIN">ИИН</label>
                         <input type="number" name="IIN" id="IIN" class="outline-none h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                     </div>
 
-                    <div class="md:col-span-2">
+                    <div className="md:col-span-2">
                         <label for="code">КОД</label>
                         <input type="number" name="code" id="code" class="outline-none h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="" />
                     </div>
@@ -54,15 +54,15 @@ export default () => {
                                 placeholder="Прикрепите договор"></input>
                     </div>
 
-                    <div class="md:col-span-2 text-left">
-                        <div class="inline-flex items-end">
-                        <button onClick={() => navigate("/panel")} class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Отменить</button>
+                    <div className="md:col-span-2 text-left">
+                        <div className="inline-flex items-end">
+                        <button onClick={() => navigate("/panel")} class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl">Отменить</button>
                         </div>
                     </div>
 
-                    <div class="md:col-span-2 text-right">
-                        <div class="inline-flex items-end">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Добавить</button>
+                    <div className="md:col-span-2 text-right">
+                        <div className="inline-flex items-end">
+                        <button className="bg-[#007282] hover:bg-[#005F6D] text-white font-bold py-2 px-4 rounded-xl">Добавить</button>
                         </div>
                     </div>
 
